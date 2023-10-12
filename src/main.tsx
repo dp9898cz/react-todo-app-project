86111f93083e87078f8ab18bc523e4cf14596dbd
dowl <xpatek08@stud.fit.vutbr.cz>
2023-10-12 17:42:57 +0200

    added base

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename={import.meta.env.DEV ? '/' : '/react-todo-app-project/'}>
                 <MainRouter />
             </BrowserRouter>
         </Provider>
